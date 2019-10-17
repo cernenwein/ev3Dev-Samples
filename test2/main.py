c9cb3232249878bd6645fa4516fd62143c4370c1
Christopher Ernenwein <chris.ernenwein@gmail.com>
2019-10-17 19:09:27 -0400

    more

--- test2/main.py
+++ test2/main.py
@@ -10,3 +10,19 @@ from pybricks.robotics import DriveBase
 
 # Write your program here
 brick.sound.beep()
+
+# Initialize two motors with default settings on Port B and Port C.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+# The wheel diameter of the Robot Educator is 56 millimeters.
+# The distance between wheels (axle_track) is 114 millimeters.
+wheel_diameter = 56
+axle_track = 114
+# Create a DriveBase object. The wheel_diameter and axle_track values are
+# needed to move robot correct speed/distance when you give drive commands.
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+
+# This drives at 100 mm/sec straight
+robot.drive (100, 0)
+# This drives straight backwards at 500 mm/sec for 2 seconds
+robot.drive_time(-500, 0, 2000) 
